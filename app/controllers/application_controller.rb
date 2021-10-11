0fa60812e2fb4621db05421b1c1dc8d30eeb2ddf
Nicholo <nicholo@intelimina.com>
2021-10-11 18:17:12 +0800

    Update application controller to rescue RecordNotFound errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+  respond_to :json
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    respond_to do |format|
+      format.json { render json: { errors: exception.message }, status: :not_found }
+    end
+  end
 end
